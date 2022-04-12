07486c8da8a967ad0b39fb5096da03088b4bf582
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-13 01:31:46 +0400

    created c sharp file (homework)

---
+++ Ededin tek ve ya cut oldughunu tapan alqoritm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ededin_tek_ve_ya_cut_oldughunu_tapan_alqoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            Console.WriteLine("Eded daxil edin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.WriteLine("Eded cutdur");
+
+            }
+            else
+            {
+                Console.WriteLine("Eded tekdir");
+            }
+        }
+    }
+}
